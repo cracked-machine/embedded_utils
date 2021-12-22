ee76e4a9003836f78e1a60f4f1229c45f55c80a4
cracked-machine <chrisjsutton@gmail.com>
2021-12-22 23:54:36 +0000

    modified spi utils to use new microtimer

--- inc/ll_spi_utils.hpp
+++ inc/ll_spi_utils.hpp
@@ -20,12 +20,16 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#ifndef __LL_SPI_UTILS_HPP__
+#define __LL_SPI_UTILS_HPP__
 
+#include "ll_tim_utils.hpp"
 
 namespace embedded_utils
 {
 
 // @brief common functions for using with STM32 LL SPI
+
 class LowLevelSPIUtils
 {
 
@@ -35,59 +39,66 @@ public:
 
     // @brief Check and retry (with timeout) the SPIx_SR TXE register.
     // static resolved performance drop after moving this code to this external class
-    // @param spi_handle Pointer to the STM32 LL SPI_TypeDef object
-    // @param delay_ms The timeout
+    // @param spi_handle Pointer to the STM32 LL SPI_HANDLE object
+    // @param delay_us The timeout
     // @return true if TX FIFO is empty, false if TX FIFO is full
-    static bool check_txe_flag_status(const SPI_TypeDef *spi_handle, uint32_t delay_ms = 1)
-    {
-
-        if (spi_handle == nullptr)
-        {
-            return false;
-        }
-
-        // The TXE flag is set when transmission TXFIFO has enough space to store data to send.
-        //if (LL_SPI_IsActiveFlag_TXE(spi_handle) == 0)
-        if ((spi_handle->SR & SPI_SR_TXE) != (SPI_SR_TXE))
-        {
-            // give TX FIFO a chance to clear before checking again
-            LL_mDelay(delay_ms);
-            //if (LL_SPI_IsActiveFlag_TXE(spi_handle) == 0)
-            if ((spi_handle->SR & SPI_SR_TXE) != (SPI_SR_TXE))
-            { 
-                return false;
-            }
-            
-        }
-        return true;
-    }        
+    template<typename SPI_HANDLE>
+    static bool check_txe_flag_status(const SPI_HANDLE *spi_handle, uint32_t delay_us = 100);
 
     // @brief Check and retry (with timeout) the SPIx_SR BSY register.
     // static resolved performance drop after moving this code to this external class
-    // @param spi_handle Pointer to the STM32 LL SPI_TypeDef object
-    // @param delay_ms The timeout
+    // @param spi_handle Pointer to the STM32 LL SPI_HANDLE object
+    // @param delay_us The timeout
     // @return true if SPI bus is busy, false if SPI bus is not busy.
-    static bool check_bsy_flag_status(const SPI_TypeDef *spi_handle, uint32_t delay_ms = 1)
-    {
-        if (spi_handle == nullptr)
-        {
-            return false;
-        }
-        // When BSY is set, it indicates that a data transfer is in progress on the SPI
-        // if (LL_SPI_IsActiveFlag_BSY(SPI_HANDLE)) 
-        if ((spi_handle->SR & SPI_SR_BSY) == (SPI_SR_BSY))
-        {
-            // give SPI bus a chance to finish sending data before checking again
-            LL_mDelay(delay_ms);
-            //if (LL_SPI_IsActiveFlag_BSY(SPI_HANDLE)) 
-            if ((spi_handle->SR & SPI_SR_BSY) == (SPI_SR_BSY))
-            { 
-                return false;
-            }
-        }    
-        return true; 
-    }        
+    template<typename SPI_HANDLE>
+    static bool check_bsy_flag_status(const SPI_HANDLE *spi_handle, uint32_t delay_us = 100);
 
 };
 
+template<typename SPI_HANDLE>
+bool LowLevelSPIUtils::check_txe_flag_status(const SPI_HANDLE *spi_handle, uint32_t delay_us)
+{
+
+    if (spi_handle == nullptr)
+    {
+        return false;
+    }
+
+    // The TXE flag is set when transmission TXFIFO has enough space to store data to send.
+    if ((spi_handle->SR & SPI_SR_TXE) != (SPI_SR_TXE))
+    {
+        // give TX FIFO a chance to clear before checking again
+        ll_uDelay::timeout(TIM14, delay_us);
+        if ((spi_handle->SR & SPI_SR_TXE) != (SPI_SR_TXE))
+        { 
+            return false;
+        }
+        
+    }
+    return true;
+}        
+
+template<typename SPI_HANDLE>
+bool LowLevelSPIUtils::check_bsy_flag_status(const SPI_HANDLE *spi_handle, uint32_t delay_us)
+{
+    if (spi_handle == nullptr)
+    {
+        return false;
+    }
+    // When BSY is set, it indicates that a data transfer is in progress on the SPI
+    if ((spi_handle->SR & SPI_SR_BSY) == (SPI_SR_BSY))
+    {
+        // give SPI bus a chance to finish sending data before checking again
+        ll_uDelay::timeout(TIM14, delay_us);
+        if ((spi_handle->SR & SPI_SR_BSY) == (SPI_SR_BSY))
+        { 
+            return false;
+        }
+    }    
+    return true; 
+}   
+
 } // namespace embedded_utils
+
+
+#endif // __LL_SPI_UTILS_HPP__
