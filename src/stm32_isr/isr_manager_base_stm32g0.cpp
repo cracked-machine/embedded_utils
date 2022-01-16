835d6020011125946b3e6e5193a9ead8bef9eaf2
cracked-machine <chrisjsutton@gmail.com>
2022-01-16 12:16:24 +0000

    added ISR functions defs with #ifdefs

--- src/stm32_isr/isr_manager_base_stm32g0.cpp
+++ src/stm32_isr/isr_manager_base_stm32g0.cpp
@@ -32,34 +32,170 @@
 namespace isr::stm32g0
 {
 
-void ISRManagerBaseSTM32G0::register_handler(InterruptList interrupt_number, std::unique_ptr<ISRManagerBaseSTM32G0> &interrupt_manager_instance)
+void ISRManagerBaseSTM32G0::register_handler(InterruptType interrupt_type, std::unique_ptr<ISRManagerBaseSTM32G0> &interrupt_manager_instance)
 {
-    assert(static_cast<size_t>(interrupt_number) < ISRManagerBaseSTM32G0::ISRVectorTable.size());
+    assert(static_cast<size_t>(interrupt_type) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
 
     // add the interrupt manager instance to the array for the specified interrupt number
-    ISRVectorTable[ static_cast<int>(interrupt_number) ] = std::move(interrupt_manager_instance);
+    m_interrupt_handlers[ static_cast<int>(interrupt_type) ] = std::move(interrupt_manager_instance);
 }
 
-extern "C" void EXTI4_15_IRQHandler(void)
-{
-    // Do not assign a value to ISRManagerBaseSTM32G0::InterruptList::list_count
-    static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptList::exti5) < ISRManagerBaseSTM32G0::ISRVectorTable.size());
+////
+//// Interrupt function definitions 
+////
 
-    // call the ISR() of the registered interrupt class
-    if (LL_EXTI_IsActiveFallingFlag_0_31(LL_EXTI_LINE_5))
+#ifdef ENABLE_EXTI_ISR
+    extern "C" void EXTI4_15_IRQHandler(void)
     {
-        ISRManagerBaseSTM32G0::ISRVectorTable[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptList::exti5) ]->ISR();
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::exti5) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+
+        // call the ISR() of the registered interrupt class
+        if (LL_EXTI_IsActiveFallingFlag_0_31(LL_EXTI_LINE_5))
+        {
+            ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::exti5) ]->ISR();
+        }
     }
-}
+#endif // ENABLE_EXTI_ISR
 
-extern "C" void DMA1_Channel1_IRQHandler(void)
-{
-    // Do not assign a value to ISRManagerBaseSTM32G0::InterruptList::list_count
-    static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptList::dma_ch1) < ISRManagerBaseSTM32G0::ISRVectorTable.size());
-    
-    ISRManagerBaseSTM32G0::ISRVectorTable[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptList::dma_ch1) ]->ISR();
-}
+#ifdef ENABLE_DMA_ISR
+    extern "C" void DMA1_Channel1_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::dma_ch1) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::dma_ch1) ]->ISR();
+    }
+#endif // ENABLE_DMA_ISR
 
+#ifdef ENABLE_I2C_ISR
+    extern "C" void I2C1_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();                
+    }                                          
+    extern "C" void I2C2_3_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();                
+    }                             
+#endif
 
+#ifdef ENABLE_SPI_ISR
+    // add SPI interrupt function definitions here
+#endif // ENABLE_SPI_ISR
+
+#ifdef ENABLE_TIM1_ISR
+    extern "C" void TIM1_BRK_UP_TRG_COM_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+    extern "C" void TIM1_CC_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM1_ISR
+#ifdef ENABLE_TIM2_ISR
+    extern "C" void TIM2_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM2_ISR
+#ifdef ENABLE_TIM3_TIM4_ISR
+    extern "C" void TIM3_TIM4_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM3_TIM4_ISR
+#ifdef ENABLE_TIM6_ISR
+    extern "C" void TIM6_DAC_LPTIM1_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM6_ISR
+#ifdef ENABLE_TIM7_LPTIM2_ISR
+    extern "C" void TIM7_LPTIM2_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM7_LPTIM2_ISR
+#ifdef ENABLE_TIM14_ISR
+    extern "C" void TIM14_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM14_ISR
+#ifdef ENABLE_TIM15_ISR
+    extern "C" void TIM15_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM15_ISR    
+#ifdef ENABLE_TIM16_FDCAN_IT0_ISR
+    extern "C" void TIM16_FDCAN_IT0_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM16_ISR
+#ifdef ENABLE_TIM17_FDCAN_IT1_ISR
+    extern "C" void TIM17_FDCAN_IT1_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();     
+    }
+#endif // ENABLE_TIM17_FDCAN_IT1_ISR
+
+#ifdef ENABLE_ADC_COMP_ISR
+    extern "C" void ADC1_COMP_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();                
+    }
+#endif // ENABLE_ADC_COMP_ISR
+
+#ifdef ENABLE_USB_ISR
+    extern "C" void USB_UCPD1_2_IRQHandler(void)
+    {
+        // Do not assign a value to ISRManagerBaseSTM32G0::InterruptType::list_count
+        static_assert( static_cast<std::size_t>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) < ISRManagerBaseSTM32G0::m_interrupt_handlers.size());
+        // call the ISR function of the associated instance
+        ISRManagerBaseSTM32G0::m_interrupt_handlers[ static_cast<int>(ISRManagerBaseSTM32G0::InterruptType::ADD_INTERRUPT_TYPE_HERE) ]->ISR();        
+    }
+#endif // ENABLE_USB_ISR
 
 } // namespace isr::stm32g0
