d18c2b609f8e07eabc37067dc18bd2a061ee88b3
cracked-machine <chrisjsutton@gmail.com>
2022-01-23 02:25:44 +0000

    added static allocated map container

---
+++ inc/static_map.hpp
@@ -0,0 +1,59 @@
+// MIT License
+
+// Copyright (c) 2021 Chris Sutton
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#ifndef __STATIC_MAP_HPP__
+#define __STATIC_MAP_HPP__
+
+namespace noarch::containers
+{
+
+// @brief Associative container with contains key-value pairs that is allocated at compile-time
+// @tparam Key The Key
+// @tparam Value The Value
+// @tparam Size The size of the map/number of Key/Value pairs. Must be constant.
+template <typename Key, typename Value, std::size_t Size>
+struct StaticMap {
+    
+    // @brief The dictionary
+    std::array<std::pair<Key, Value>, Size> data;
+
+    // @brief access specified element
+    // @param key The key element to match
+    // @return Value* Pointer to the value element, or nullptr if not found
+    Value* find_key(const Key &key) {
+
+        // @brief if key is found return iterator to array item 
+        auto itr = std::find_if(begin(data), end(data), [&key](auto &v) 
+        { 
+            return v.first == key; 
+        });
+        // return the value if the key was found
+        if (itr != end(data)) { return &itr->second; } 
+        // or return nullptr as the search completed without match
+        return nullptr;
+    }
+};
+
+} // namespace noarch::static_map
+
+#endif // __STATIC_MAP_HPP__
