360d706b19bdde8ca9eaf195928fb242e89354fe
cracked-machine <chrisjsutton@gmail.com>
2022-03-18 13:54:00 +0000

    suppressed explicit ctor warning

--- inc/static_string.hpp
+++ inc/static_string.hpp
@@ -49,6 +49,8 @@ public:
 
     /// @brief Construct with literal string.
     /// @param str The c-string literal input. Must be of length = CAPACITY-1
+    // Allow implicit conversion from 'const char [CAPACITY]' to 'StaticString<CAPACITY>&'
+    // cppcheck-suppress noExplicitConstructor
     StaticString(const char (&str)[CAPACITY])
     {
         std::memcpy(m_string.data(), str, m_string.size());
