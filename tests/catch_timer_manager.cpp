24af651aee2d6a057b8975442cbed4cca642b734
cracked-machine <chrisjsutton@gmail.com>
2022-03-27 17:03:37 +0100

    updated comments

--- tests/catch_timer_manager.cpp
+++ tests/catch_timer_manager.cpp
@@ -7,7 +7,10 @@
 #include <future>
 
 
-
+/// @brief Simulate the SysTick counter (normally done by ARM HW).
+/// See "Cortex-M0 Technical Ref Man - SysTick Control and Status Register"
+/// @param systick 
+/// @return false if null input, return true when fixture has terminated
 bool testfixture_systick_sim(SysTick_Type *systick)
 {
     using namespace std::chrono_literals;
@@ -28,7 +31,8 @@ bool testfixture_systick_sim(SysTick_Type *systick)
     return true;
 }
 
-/// @brief Update the timer counter (normally done by STM32 HW) when CR1 register is enabled 
+/// @brief Simulate the timer counter (normally done by STM32 HW) 
+/// See STM32 Reference Manal Timer control register 1
 /// @param timer The mocked STM32 registers
 /// @return false if timer is null
 bool testfixture_timer_sim(TIM_TypeDef *timer)
